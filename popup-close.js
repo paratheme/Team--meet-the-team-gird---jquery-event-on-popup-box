7713427acda9458fceabae8421045e306abcc5cf
nurhasan <public.nurhasan@gmail.com>
2015-05-03 07:20:49 +0600

    upload popup-close.js

---
+++ popup-close.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+jQuery(document).ready(function($)
+	{
+		
+		
+		$(document).on('click', '.team-slide .x-icon-play', function()
+			{	
+				$('.team-popup-box').fadeOut();
+				$('.team-slide').fadeOut();
+				
+			})	
+			
+	})
